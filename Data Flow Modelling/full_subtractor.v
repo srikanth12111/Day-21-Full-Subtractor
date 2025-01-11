5773e4734d2cbd8fbe062fccb05c9d77ad0b4d0f
srikanth12111 <148364699+srikanth12111@users.noreply.github.com>
2025-01-11 18:16:28 +0530

    Create full_subtractor.v

---
+++ Data Flow Modelling/full_subtractor.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+
+//Implementaion of FULL SUBTRACTOR by using Data Flow Modelling
+module full_subtractor(
+    input a,
+    input b,
+    input cin,
+    output  Diff,Barrow
+    );
+assign Diff=a^b^cin;
+assign Barrow=(~(a^b))&cin|~a&b;
+endmodule
